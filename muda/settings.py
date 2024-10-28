d8257132c3a50d0336e032f1fe79006e324a84ff
MohammadHossein <84839113+mmdhossein79@users.noreply.github.com>
2024-10-28 15:02:07 +0330

    Update settings.py

--- muda/settings.py
+++ muda/settings.py
@@ -143,8 +143,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'mudawears@gmail.com'
-EMAIL_HOST_PASSWORD = 'jmka ogsr dwbw jjhz'
+EMAIL_HOST_USER = '####@gmail.com'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 
